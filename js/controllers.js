acad82c0ac1ccd1d4c700934639861fd909d956f
Uchi Uchibeke <uchi.u@usask.ca>
2016-10-22 15:29:17 -0400

    Add new changes

--- js/controllers.js
+++ js/controllers.js
@@ -21,8 +21,58 @@ function($rootScope, $scope, $http, $localStorage, $timeout, $interval) {
 	$scope.hey = "___";
 
 	$scope.$storage.x = '';
-	
-	
 
+	$scope.start = function() {
+		var mode = getMode();
+		var luisCfg = getLuisConfig();
 
+		var listenDur = 2000;
+
+		clearText();
+
+		if (useMic()) {
+			if (luisCfg) {
+				client = Microsoft.CognitiveServices.SpeechRecognition.SpeechRecognitionServiceFactory.createMicrophoneClientWithIntent(getLanguage(), getKey(), luisCfg.appid, luisCfg.subid);
+				console.log("using 1");
+			} else {
+				client = Microsoft.CognitiveServices.SpeechRecognition.SpeechRecognitionServiceFactory.createMicrophoneClient(mode, getLanguage(), getKey());
+				console.log("using 2 " + mode);
+			}
+			client.startMicAndRecognition();
+			setTimeout(function() {
+				$scope.cgColor = !$scope.cgColor;
+				client.endMicAndRecognition();
+			}, listenDur);
+		} else {
+			if (luisCfg) {
+				client = Microsoft.CognitiveServices.SpeechRecognition.SpeechRecognitionServiceFactory.createDataClientWithIntent(getLanguage(), getKey(), luisCfg.appid, luisCfg.subid);
+			} else {
+				client = Microsoft.CognitiveServices.SpeechRecognition.SpeechRecognitionServiceFactory.createDataClient(mode, getLanguage(), getKey());
+			}
+			request = new XMLHttpRequest();
+			request.open('GET', (mode == Microsoft.CognitiveServices.SpeechRecognition.SpeechRecognitionMode.shortPhrase) ? "whatstheweatherlike.wav" : "batman.wav", true);
+			request.responseType = 'arraybuffer';
+			request.onload = function() {
+				if (request.status !== 200) {
+					setText("unable to receive audio file");
+				} else {
+					client.sendAudio(request.response, request.response.length);
+				}
+			};
+			request.send();
+		}
+
+		client.onPartialResponseReceived = function(response) {
+			setText(JSON.stringify(response));
+			console.log(JSON.stringify(response));
+		}
+
+		client.onFinalResponseReceived = function(response) {
+			setText(JSON.stringify(response));
+		}
+
+		client.onIntentReceived = function(response) {
+			setText(JSON.stringify(response));
+		};
+	};
 }]);
